e49167b4f65229433bdb16a602f5a042791a8233
Jason Stajich <jasonstajich.phd@gmail.com>
2022-03-24 23:22:37 -0700

    launch steP

--- pipeline/asm/01_launch_canu.sh
+++ pipeline/asm/01_launch_canu.sh
@@ -8,7 +8,7 @@ SAMPLES=samples.csv
 OUTDIR=asm/canu
 INDIR=data/Nanopore
 mkdir -p $OUTDIR
-while read STRAIN NANOPORE ILLUMINA
+while read STRAIN NANOPORE ILLUMINA LOCUS
 do
     canu -p $STRAIN -d $OUTDIR/$STRAIN genomeSize=30m useGrid=true -nanopore $INDIR/$NANOPORE
 done < $SAMPLES
