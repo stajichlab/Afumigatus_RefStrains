3707803dc4d6a022a7bc064d5c4886a4dad55c3d
Jason Stajich <jasonstajich.phd@gmail.com>
2022-03-26 15:22:56 -0700

    ref genome download

---
+++ ref_genomes/download.sh
@@ -0,0 +1,2 @@
+curl -C - -O https://fungidb.org/common/downloads/release-56/AfumigatusA1163/fasta/data/FungiDB-56_AfumigatusA1163_Genome.fasta
+curl -C - -O https://fungidb.org/common/downloads/release-56/AfumigatusAf293/fasta/data/FungiDB-56_AfumigatusAf293_Genome.fasta
