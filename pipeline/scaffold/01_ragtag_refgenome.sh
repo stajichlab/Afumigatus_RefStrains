64f0dd6d61d58cf7b4ab75e3f5b733a3d059dba6
Jason Stajich <jasonstajich.phd@gmail.com>
2022-03-27 14:28:17 -0700

    add quick merge step for testing

---
+++ pipeline/scaffold/01_ragtag_refgenome.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash -l
+#SBATCH -p short -N 1 -n 4  --mem 4gb --out logs/ragtag_scaffold.log
+
+module load ragtag
+CPUS=$SLURM_CPUS_ON_NODE
+if [ -z $CPUS ]; then
+ CPUS=1
+fi
+
+
+IFS=,
+SAMPLES=samples.csv
+INDIR=genomes
+OUTDIR=asm/ref_scaffold
+AF293=ref_genomes/FungiDB-56_AfumigatusAf293_Genome.fasta
+A1163=ref_genomes/FungiDB-56_AfumigatusA1163_Genome.fasta
+mkdir -p $OUTDIR
+cat $SAMPLES | while read STRAIN NANOPORE ILLUMINA LOCUS
+do
+    for type in canu flye
+    do
+	if [ ! -f $INDIR/$STRAIN.$type.pilon.fasta ]; then
+		echo "Cannot find $INDIR/$STRAIN.$type.pilon.fasta"
+		exit
+	fi
+	echo "out=>$OUTDIR/$STRAIN.$type.A1163 ref=$A1163 in=$INDIR/$STRAIN.$type.pilon.fasta"
+
+	ragtag.py scaffold -t $CPUS -o $OUTDIR/$STRAIN.$type.A1163 $A1163 $INDIR/$STRAIN.$type.pilon.fasta
+	ragtag.py scaffold -t $CPUS -o $OUTDIR/$STRAIN.$type.AF293 $AF293 $INDIR/$STRAIN.$type.pilon.fasta
+    done
+done
+
---
+++ pipeline/scaffold/02_quickmerge.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash -l
+#SBATCH -p short -N 1 -n 4  --mem 64gb --out logs/quickmerge.%a.log -a 1
+
+module load quickmerge
+module load mummer/3.23
+
+N=${SLURM_ARRAY_TASK_ID}
+if [ -z $N ]; then
+    N=$1
+    if [ -z $N ]; then
+	echo "no value for SLURM ARRAY - specify with -a or cmdline"
+    fi
+fi
+
+CPU=$SLURM_CPUS_ON_NODE
+if [ -z $CPU ]; then
+	CPU=1
+fi
+
+
+IFS=,
+SAMPLES=samples.csv
+INDIR=genomes
+OUTDIR=asm/quick_merge
+AF293=ref_genomes/FungiDB-56_AfumigatusAf293_Genome.fasta
+A1163=ref_genomes/FungiDB-56_AfumigatusA1163_Genome.fasta
+mkdir -p $OUTDIR
+sed -n ${N}p $SAMPLES | while read STRAIN NANOPORE ILLUMINA LOCUS
+do
+   echo "line is $STRAIN $NANOPORE"
+
+   mkdir -p $OUTDIR/$STRAIN
+   CANU=$(realpath $INDIR/$STRAIN.canu.pilon.fasta)
+   FLYE=$(realpath $INDIR/$STRAIN.flye.pilon.fasta)
+   pushd $OUTDIR/$STRAIN
+   merge_wrapper.py -l 100 $CANU $FLYE
+   popd
+done
