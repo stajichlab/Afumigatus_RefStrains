7f2014ef08be1c8bc1df36614227f501e1b1af10
Jason Stajich <jasonstajich.phd@gmail.com>
2026-02-13 16:52:37 -0800

    runtime fix

--- NCBI/genomes/Aspergillus_fumigatus_eAF1090/make_sqn.sh
+++ NCBI/genomes/Aspergillus_fumigatus_eAF1090/make_sqn.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash -l
-#SBATCH -p short -c  2 --mem 2gb -N 1 -n 1 
+#SBATCH -p short -c  2 --mem 2gb -N 1 -n 1  -C cascade
 
 module load ncbi-table2asn
 NAME=$(basename `pwd`)
 
 if [ ! -s $NAME.fsa ]; then
- mv *.fa $NAME.fsa
+ mv $NAME.fa $NAME.fsa
 fi
 
 table2asn -l paired-ends -V v -M n -c ef -i $NAME.fsa -o $NAME.sqn -Z -euk -t ../../../lib/sbt/Afum.sbt -j "[organism=Aspergillus fumigatus] [strain=eAF1090] [gcode=1]"
